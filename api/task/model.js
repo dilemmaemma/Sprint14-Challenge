c31a90b5b2a5ba30d693cc245111631bf0b8cae1
delimmaemma <calvinhtml@gmail.com>
2023-09-01 22:05:03 -0500

    Can now change completed tasks/projects to boolean

--- api/task/model.js
+++ api/task/model.js
@@ -1,6 +1,26 @@
 // build your `Task` model here
 const db = require('../../data/dbConfig')
 
-exports.getAll = function () {
-    return db('tasks')
+exports.getAll = async function () {
+    const result = await db('tasks as t')
+        .leftJoin('projects as p', 't.project_id', 'p.project_id')
+        .select([
+            'task_id',
+            'task_description',
+            'task_notes',
+            'task_completed',
+            'project_name',
+            'project_description',
+            'project_completed'
+        ])
+        .then((rows) => {
+            const modifiedRows = rows.map(row => ({
+                ...row,
+                task_completed: row.task_completed === 1 ? true : false,
+                project_completed: row.project_completed === 1 ? true : false
+            }))
+            return modifiedRows
+        })
+    
+    return result
 }
