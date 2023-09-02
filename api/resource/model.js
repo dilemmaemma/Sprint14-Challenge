11abdd1a4856f551b8e131b4961f9b38a364e295
delimmaemma <calvinhtml@gmail.com>
2023-09-02 02:20:58 -0500

    Finished Resource folder

--- api/resource/model.js
+++ api/resource/model.js
@@ -4,3 +4,9 @@ const db = require('../../data/dbConfig')
 exports.getAll = function () {
     return db('resources')
 }
+exports.add = function (resource) { 
+    return db('resources').insert(resource)
+    .then(([resource_id]) => {
+      return db('resources').where('resource_id', resource_id).first()
+    })
+}
